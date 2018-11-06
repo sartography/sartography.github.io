485fa25ebd291e3146844be9e267d9afb2911260
eleanor <eleanorcjkgraham@gmail.com>
2018-11-06 16:31:09 -0500

    add place-holder components for about us, contact us, case studies, and reviews

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,22 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { CaseStudiesComponent } from './case-studies/case-studies.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { ReviewsComponent } from './reviews/reviews.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AboutUsComponent,
+    CaseStudiesComponent,
+    ContactUsComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    ReviewsComponent
   ],
   imports: [
     BrowserModule,
