d081fd07b11919bd48eb792bee8cd98edcc641c4
eleanor <eleanorcjkgraham@gmail.com>
2018-11-12 10:26:54 -0500

    get team member information for about page from json. add buttons for github and linkedin profiles to team member cards

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TeamMember } from '../interfaces';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-about-us',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-us.component.scss']
 })
 export class AboutUsComponent implements OnInit {
+  teamMembers: TeamMember[];
 
-  constructor() { }
+  constructor(
+    private api: ApiService
+  ) { }
 
   ngOnInit() {
+    this.api.getTeamMembers().subscribe(team => {
+      this.teamMembers = team;
+    });
+  }
+
+  goWebsite(link: string) {
+    window.open(link, '_blank');
   }
 
 }
